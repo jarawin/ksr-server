47fdcd446670955932ac2e9c486cea35aedf8906
jarawin <newja2021@gmail.com>
2024-11-23 14:21:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import {
+  checkPrinterStatus,
+  printPreReceipt,
+  printReceipt,
+} from "./src/printer";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+app.post("/print-receipt", async (req, res) => {
+  try {
+    const { receiptData } = req.body;
+    await printReceipt(receiptData);
+    res.status(200).send("Print job sent successfully.");
+  } catch (error) {
+    console.error("Error printing:", error);
+    res.status(500).send("Printing failed.");
+  }
+});
+
+app.post("/print-pre-receipt", async (req, res) => {
+  try {
+    const { receiptData } = req.body;
+    await printPreReceipt(receiptData);
+    res.status(200).send("Print job sent successfully.");
+  } catch (error) {
+    console.error("Error printing:", error);
+    res.status(500).send("Printing failed.");
+  }
+});
+
+app.get("/status", async (req, res) => {
+  try {
+    const isConnected = await checkPrinterStatus();
+    res.status(200).send({ connected: isConnected });
+  } catch (error) {
+    console.error("Error checking printer status:", error);
+    res.status(500).send("Error checking printer status.");
+  }
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ src/printer.js
@@ -0,0 +1,308 @@
+import {
+  ThermalPrinter,
+  PrinterTypes,
+  CharacterSet,
+} from "node-thermal-printer";
+
+const printer = new ThermalPrinter({
+  interface: `tcp://${process.env.PRINTER_HOST}:${process.env.PRINTER_PORT}`,
+  characterSet: CharacterSet.WPC1252,
+  type: PrinterTypes.EPSON,
+  //   interface: "tcp://ksrcarclean.thddns.net:7372",
+  //   interface: "tcp://promsawat.thddns.net:4640",
+});
+
+import fs from "fs";
+const logoBuffer = fs.readFileSync("../public/ksrlogo.png");
+
+const lineFeed = () =>
+  JSON.parse(
+    JSON.stringify([
+      {
+        text: "------------------------------------------------------",
+        align: "CENTER",
+        width: 1.15,
+      },
+    ])
+  );
+
+async function printPreReceipt(receiptData) {
+  printer.setTypeFontB();
+  printer.alignCenter();
+
+  //? Print Header
+  printer.printImageBuffer(logoBuffer);
+  printer.bold(true);
+  printer.println("เคเอสอาร์คาร์คลีน สาขาโลตัสสงขลา");
+  printer.bold(false);
+  printer.tableCustom([
+    {
+      text: "เปิดทุกวัน 08:30-18:30 น. ติดต่อ 065-4068840",
+      width: 1.2,
+      align: "CENTER",
+    },
+  ]);
+  printer.tableCustom([
+    {
+      text: "ให้บริการล้างสีดูดฝุ่น เคลือบสี ขัดสี ฟอกเบาะ เคลือบแก้ว",
+      width: 1.2,
+      align: "CENTER",
+    },
+  ]);
+  printer.println();
+  printer.tableCustom(lineFeed());
+
+  //? Print Sub Header
+  const {
+    billNo,
+    customerPhone,
+    receiveDate,
+    licensePlate,
+    carDetail,
+    carSize,
+    paymentStatus,
+  } = receiptData.header;
+
+  printer.alignLeft();
+  printer.println(`เลขที่บิล: ${billNo}`);
+  printer.println(`เบอร์โทรลูกค้า: ${customerPhone}`);
+  printer.println(`เวลารับรถ: ${receiveDate} น.`);
+  printer.println(`เวลาคืนรถ: -`);
+  printer.println(`ป้ายทะเบียน: ${licensePlate}`);
+  printer.println(`รายละเอียดรถ: ${carDetail}`);
+  printer.println(`ขนาดรถ: ${carSize}`);
+  printer.println(`การชำระเงิน: ${paymentStatus}`);
+  printer.println();
+
+  //? Print Body
+  const { services, discount, total, serviceItems } = receiptData.body;
+
+  printer.tableCustom([
+    { text: "รายการ", align: "CENTER", width: 1, bold: true },
+    { text: "ราคา", align: "CENTER", width: 0.2, bold: true },
+  ]);
+  printer.tableCustom(lineFeed());
+  services.forEach((service, index) => {
+    const { name, price } = service;
+    printer.tableCustom([
+      { text: `${index + 1}. ${name}`, align: "LEFT", width: 0.8 },
+      { text: `${price}.-`, align: "RIGHT", width: 0.2 },
+    ]);
+  });
+  printer.tableCustom(lineFeed());
+  if (discount) {
+    printer.tableCustom([
+      { text: "ส่วนลด", align: "RIGHT", width: 0.8 },
+      { text: `${discount}.-`, align: "RIGHT", width: 0.25 },
+    ]);
+  }
+  printer.tableCustom([
+    { text: "ยอดรวมสุทธิ", align: "RIGHT", width: 0.8, bold: true },
+    { text: `${total}.-`, align: "RIGHT", width: 0.25, bold: true },
+  ]);
+
+  printer.println();
+  printer.tableCustom(lineFeed());
+  printer.println();
+
+  //? Print Sub Body
+  printer.alignCenter();
+  printer.bold(true);
+  printer.println("รายการบริการที่ดำเนินการวันนี้");
+  printer.bold(false);
+  printer.println(serviceItems.join(", "));
+
+  printer.println();
+  printer.tableCustom(lineFeed());
+  printer.println();
+
+  printer.tableCustom([
+    {
+      text: "กรุณานำใบรับรถมาด้วยเมื่อมารับรถ เพื่อความสะดวกในการตรวจสอบ",
+      width: 1.3,
+      align: "CENTER",
+    },
+  ]);
+  printer.cut();
+  await printer.execute();
+}
+
+async function printReceipt(receiptData) {
+  printer.openCashDrawer();
+  printer.setTypeFontB();
+  printer.alignCenter();
+
+  //? Print Header
+  printer.printImageBuffer(logoBuffer);
+  printer.bold(true);
+  printer.println("เคเอสอาร์คาร์คลีน สาขาโลตัสสงขลา");
+  printer.bold(false);
+  printer.tableCustom([
+    {
+      text: "เปิดทุกวัน 08:30-18:30 น. ติดต่อ 065-4068840",
+      width: 1.2,
+      align: "CENTER",
+    },
+  ]);
+  printer.tableCustom([
+    {
+      text: "ให้บริการล้างสีดูดฝุ่น เคลือบสี ขัดสี ฟอกเบาะ เคลือบแก้ว",
+      width: 1.2,
+      align: "CENTER",
+    },
+  ]);
+  printer.println();
+  printer.tableCustom(lineFeed());
+
+  //? Print Sub Header
+  const {
+    billNo,
+    customerPhone,
+    receiveDate,
+    returnDate,
+    licensePlate,
+    carDetail,
+    carSize,
+    paymentStatus,
+  } = receiptData.header;
+
+  printer.alignLeft();
+  printer.println(`เลขที่บิล: ${billNo}`);
+  printer.println(`เบอร์โทรลูกค้า: ${customerPhone}`);
+  printer.println(`เวลารับรถ: ${receiveDate} น.`);
+  printer.println(`เวลาคืนรถ: ${returnDate} น.`);
+  printer.println(`ป้ายทะเบียน: ${licensePlate}`);
+  printer.println(`รายละเอียดรถ: ${carDetail}`);
+  printer.println(`ขนาดรถ: ${carSize}`);
+  printer.println(`การชำระเงิน: ${paymentStatus}`);
+  printer.println();
+
+  //? Print Body
+  const { services, discount, total, serviceItems, point } = receiptData.body;
+
+  printer.tableCustom([
+    { text: "รายการ", align: "CENTER", width: 1, bold: true },
+    { text: "ราคา", align: "CENTER", width: 0.2, bold: true },
+  ]);
+  printer.tableCustom(lineFeed());
+  services.forEach((service, index) => {
+    const { name, price } = service;
+    printer.tableCustom([
+      { text: `${index + 1}. ${name}`, align: "LEFT", width: 0.8 },
+      { text: `${price}.-`, align: "RIGHT", width: 0.2 },
+    ]);
+  });
+  printer.tableCustom(lineFeed());
+  if (discount) {
+    printer.tableCustom([
+      { text: "ส่วนลด", align: "RIGHT", width: 0.8 },
+      { text: `${discount}.-`, align: "RIGHT", width: 0.25 },
+    ]);
+  }
+  printer.tableCustom([
+    { text: "ยอดรวมสุทธิ", align: "RIGHT", width: 0.8, bold: true },
+    { text: `${total}.-`, align: "RIGHT", width: 0.25, bold: true },
+  ]);
+
+  printer.println();
+  printer.tableCustom(lineFeed());
+  printer.println();
+
+  //? Print Sub Body
+  printer.alignCenter();
+  printer.bold(true);
+  printer.println("รายการบริการที่ดำเนินการวันนี้");
+  printer.bold(false);
+  printer.println(serviceItems.join(", "));
+
+  printer.println();
+  printer.tableCustom(lineFeed());
+  printer.println();
+
+  //? Print Footer
+  if (point) {
+    printer.tableCustom([
+      {
+        text: `ได้รับแต้มสะสม: ${point} แต้ม`,
+        width: 1.2,
+        align: "CENTER",
+        bold: true,
+      },
+    ]);
+    printer.tableCustom([
+      {
+        text: "เมื่อสะสมครบ 2, 4, 6, 8, 10 แต้มจะได้รับสิทธิพิเศษ",
+        width: 1.2,
+        align: "CENTER",
+      },
+    ]);
+    printer.println();
+  }
+
+  //? Print Sub Footer
+  printer.printQR("https://lin.ee/bkrPE4i", { cellSize: 5 });
+  printer.tableCustom([
+    {
+      text: "ตรวจสอบและใช้สิทธิพิเศษได้ที่ แอดไลน์: @ksrcarclean",
+      width: 1.2,
+      align: "CENTER",
+    },
+  ]);
+
+  printer.println();
+  printer.tableCustom(lineFeed());
+  printer.println();
+
+  //? Print Note
+  printer.bold(true);
+  printer.println("หมายเหตุ");
+  printer.bold(false);
+  printer.tableCustom([
+    {
+      text: "เมื่อท่านรับรถคืน กรุณาตรวจสอบความเรียบร้อยของรถและ",
+      width: 1.3,
+      align: "CENTER",
+    },
+  ]);
+  printer.tableCustom([
+    {
+      text: "ทรัพย์สินภายในทันที หากพบความเสียหายหรือสิ่งของสูญหาย",
+      width: 1.3,
+      align: "CENTER",
+    },
+  ]);
+  printer.tableCustom([
+    {
+      text: "โปรดแจ้งให้ทางร้านทราบก่อนออกจากศูนย์บริการ ทั้งนี้",
+      width: 1.3,
+      align: "CENTER",
+    },
+  ]);
+  printer.tableCustom([
+    {
+      text: "ทางร้านขอสงวนสิทธิ์ในการรับผิดชอบกรณีที่ไม่ได้แจ้งในขณะนั้น",
+      width: 1.3,
+      align: "CENTER",
+    },
+  ]);
+  printer.println();
+  printer.tableCustom(lineFeed());
+  printer.println();
+
+  //? Print Thanks
+  printer.println("ขอบคุณที่ใช้บริการ เคเอสอาร์คาร์คลีน");
+  printer.cut();
+  await printer.execute();
+}
+
+async function checkPrinterStatus() {
+  try {
+    const isConnected = await printer.isPrinterConnected();
+    return isConnected;
+  } catch (error) {
+    console.error("Error checking printer status:", error);
+    return false;
+  }
+}
+
+export { printPreReceipt, printReceipt, checkPrinterStatus };
